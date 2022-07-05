e1b7809329bc74c0d31333909d6786d2cd0b2f80
Anas <a.khafaga@qoyod.com>
2022-07-05 04:03:40 +0200

    board II

--- src/models/Board.js
+++ src/models/Board.js
@@ -2,6 +2,8 @@
  * @class Board
  */
 
+import Square from './Square';
+
 class Board {
 
     constructor() {
@@ -9,7 +11,27 @@ class Board {
         this.rows = [1, 2, 3, 4, 5, 6, 7, 8];
         this.cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     }
+
+    generateSquares() {
+        const iter = this[Symbol.iterator]();
+
+        for(let [name, [col, row], color] of iter) {
+            this.boardMap.set(name, new Square(name, this.cols.indexOf(col) + 1, row, color));
+        }
+
+        return this.boardMap;
+    }
     
+    *[Symbol.iterator]() {
+        let color = true;
+        for(let row of this.rows){
+            for(let col of this.cols) {
+                yield [`${col}${row}`, [col, row], color]
+                color = !color
+            }
+            color = !color
+        }
+    }
 }
 
 export default new Board();
--- src/views/Board.js
+++ src/views/Board.js
@@ -1,13 +1,30 @@
 import Square from './Square';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import model from '../models/Board';
 
 export default function Board() {
 
     const [boardSqs, setBoardSqs] = useState([]);
     
+    useEffect(() => {
+        let boardMap = model.generateSquares();
+        let boardSqsHolder = [];
+
+        boardMap.forEach((v, k) => {
+            boardSqsHolder.push({k, v})
+        })
+
+        setBoardSqs(boardSqsHolder);
+        
+    }, [])
+    
     return (
         <div>
-            <Square />
+            {boardSqs.length && boardSqs.map(({k, v}) => {
+                return (
+                    <Square key={k} model={v}/>
+                )
+            })}
         </div>
     )
 }
