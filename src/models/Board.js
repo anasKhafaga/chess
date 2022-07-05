7bf35d28ecb23e9e28246cac6f2117086bf18104
Anas <a.khafaga@qoyod.com>
2022-07-05 04:42:42 +0200

    align armies

--- src/models/Board.js
+++ src/models/Board.js
@@ -3,6 +3,7 @@
  */
 
 import Square from './Square';
+import {Bishop, King, Queen, Pawn, Rook, Knight} from './Warriors'
 
 class Board {
 
@@ -20,6 +21,42 @@ class Board {
         }
 
         return this.boardMap;
+    }
+
+    alignArmy(map, armyColor) {
+        const army = new Map();
+
+        function setElem(sqs, WarClass, rank, name) {
+            sqs.forEach((sq, idx) => {
+                let finalName = name || `${rank}${++idx}`;
+                army.set(finalName, new WarClass({name, finalName, rank, army: armyColor, sq}))
+            })
+        }
+        
+        for(let [rank, sqs] of map) {
+            switch(rank){
+                case 'K':
+                    setElem(sqs, King, 'K', 'K');
+                    break;
+                case 'Q':
+                    setElem(sqs, Queen, 'Q', 'Q');
+                    break;
+                case 'B':
+                    setElem(sqs, Bishop, 'B');
+                    break;
+                case 'R':
+                    setElem(sqs, Rook, 'R');
+                    break;
+                case 'N':
+                    setElem(sqs, Knight, 'N');
+                    break;
+                case 'P':
+                    setElem(sqs, Pawn, 'P');
+                    break;
+            }
+        }
+
+        return army;
     }
     
     *[Symbol.iterator]() {
--- src/views/Board.js
+++ src/views/Board.js
@@ -14,6 +14,28 @@ export default function Board() {
             boardSqsHolder.push({k, v})
         })
 
+        const whiteArmyMap = new Map();
+        const blackArmyMap = new Map();
+
+        whiteArmyMap
+        .set('K', [boardMap.get('e1')])
+        .set('Q', [boardMap.get('d1')])
+        .set('B', [boardMap.get('c1'), boardMap.get('f1')])
+        .set('N', [boardMap.get('b1'), boardMap.get('g1')])
+        .set('R', [boardMap.get('a1'), boardMap.get('h1')])
+        .set('P', [boardMap.get('a2'), boardMap.get('b2'), boardMap.get('c2'), boardMap.get('d2'), boardMap.get('e2'), boardMap.get('f2'), boardMap.get('g2'), boardMap.get('h2')])
+
+        blackArmyMap
+        .set('K', [boardMap.get('e8')])
+        .set('Q', [boardMap.get('d8')])
+        .set('B', [boardMap.get('c8'), boardMap.get('f8')])
+        .set('N', [boardMap.get('b8'), boardMap.get('g8')])
+        .set('R', [boardMap.get('a8'), boardMap.get('h8')])
+        .set('P', [boardMap.get('a7'), boardMap.get('b7'), boardMap.get('c7'), boardMap.get('d7'), boardMap.get('e7'), boardMap.get('f7'), boardMap.get('g7'), boardMap.get('h7')])
+
+        model.alignArmy(whiteArmyMap, 'white');
+        model.alignArmy(blackArmyMap, 'black');
+        
         setBoardSqs(boardSqsHolder);
         
     }, [])
