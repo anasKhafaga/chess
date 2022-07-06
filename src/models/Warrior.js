770f2f21224f70feaf0a0afbffa263be066be939
Anas <a.khafaga@qoyod.com>
2022-07-06 02:00:13 +0200

    covered squares

--- src/models/Warrior.js
+++ src/models/Warrior.js
@@ -11,7 +11,16 @@ class Warrior {
         this.sq = sq;
         sq && sq.occupy(this)
         this.firstMove = true;
-        this.coveredSqs = [];
+        this.coveredSqs = new Map([['for', []], ['back', []], ['rt', []], ['lt', []], ['forRt', []], ['forLt', []], ['backLt', []], ['backRt', []]]);
+    }
+
+    updateCoveredSqs(coords, board) {
+        for(let [{col, row}, dir] of coords) {
+            const colLet = board.cols[col];
+            const sq = board.boardMap.get(`${colLet}${row}`);
+
+            this.coveredSqs.get(dir).push(sq);
+        }
     }
 }
 
--- src/models/Warriors/Pawn.js
+++ src/models/Warriors/Pawn.js
@@ -11,6 +11,38 @@ class Pawn extends Warrior {
     constructor(props) {
         super({...props, white, black});
     }
+
+    updateCoveredSqs(board) {
+        let coveredCoords = [];
+        const {col, row} = this.sq.coord;
+        let marchingRow;
+
+        if(this.army === 'white'){
+            if(row === 8) return;
+            marchingRow = row + 1;
+            if (this.firstMove){
+                coveredCoords.push([{col: col - 1, row: row + 2}, 'for'])
+            }
+        } else {
+            if(row === 1) return;
+            marchingRow = row - 1;
+            if(this.firstMove){
+                coveredCoords.push([{col: col - 1, row: row - 2}, 'for'])
+            }
+        }
+
+        if(col !== 1){
+            coveredCoords.push([{col: col - 2, row: marchingRow}, 'forLt'])
+        }
+        if(col !== 8) {
+            coveredCoords.push([{col: col, row: marchingRow}, 'forRt']);
+        }
+
+        coveredCoords.push([{col: col - 1, row: marchingRow}, 'for']);
+
+        super.updateCoveredSqs(coveredCoords, board);
+        return this.coveredSqs;
+    }
 }
 
 export default Pawn;
